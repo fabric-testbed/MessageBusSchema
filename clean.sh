05e58d02b3c08a5a4745d579b126122920bf1c2e
Komal Thareja <kthar10@renci.org>
2020-07-23 15:45:19 -0400

    ssl support

---
+++ clean.sh
@@ -0,0 +1,3 @@
+docker stop zookeeper broker1 schemaregistry
+docker rm zookeeper broker1 schemaregistry
+docker rmi confluentinc/cp-schema-registry:latest confluentinc/cp-kafka:latest confluentinc/cp-zookeeper:latest
